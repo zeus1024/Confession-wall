0bb2aad9dfeeb0ebe357ec7abed5691756d6ded7
970005689 <70957110+970005689@users.noreply.github.com>
2020-10-23 11:21:45 +0800

    密码修改接口

---
+++ php/user/pwdchange.php
@@ -0,0 +1,24 @@
+<?php
+include_once('../function.php');
+include_once('../dbconfig.php');
+include_once('../class_response.php');
+$re=new response;
+if(!checklogin()) $re->setf(0,"用户未登录");
+$username=$_SESSION['user'];
+$conn=db_init();
+$sql = "SELECT pswhash FROM udb WHERE usn = $username;";
+$retval=$conn->query($sql);
+$row = mysqli_fetch_assoc($retval);
+if (password_verify($_GET['password1'],$row[pswhash]))
+{   
+	$hash=password_hash($_GET['password2'], PASSWORD_DEFAULT);
+	$sql = "UPDATE udb SET pswhash = '$hash' WHERE usn='$username'; " ;
+	$conn->query($sql);
+	$re->sets(1,"修改成功");
+	$re->out();
+	
+}
+else
+{
+	$re->setf(0,"原密码错误");
+}
