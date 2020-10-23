22e80841eaf3c2d4f6da707e94bec2abe20a9306
970005689 <70957110+970005689@users.noreply.github.com>
2020-10-23 10:50:32 +0800

    用户模型相关接口

---
+++ php/user/logout.php
@@ -0,0 +1,16 @@
+<?php
+include_once('../function.php');
+include_once('../dbconfig.php');
+include_once('../class_response.php');
+$re=new response;
+if(!checklogin()) $re->setf(0,"用户未登录");
+$username=$_SESSION['user'];
+session_unset(); 
+session_destroy();
+setcookie("auth", "", time()-3600);
+$conn=db_init();
+$sql = "UPDATE udb SET cookie = '' WHERE usn='$username' " ;
+$conn->query($sql);
+$re->sets(1,"注销成功");
+$re->out();
+?>
