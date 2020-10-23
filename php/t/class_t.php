1c8b72ec6395fc333e2db656d566736bcd3f6a03
970005689 <70957110+970005689@users.noreply.github.com>
2020-10-23 10:51:34 +0800

    帖子模型相关接口

---
+++ php/t/class_t.php
@@ -0,0 +1,55 @@
+<?php
+
+class t_base{
+	public $tid;
+	public $type;
+	public $title;
+	public $poster;
+	public $date;
+	function bind_info($row)
+	{
+	$this->tid=$row['tid'];
+	$this->type=$row['type'];
+	$this->title=$row['title'];
+	if($row['hide_poster']) $this->poster="匿名用户";else $this->poster=$row['poster'];
+	$this->date=$row["date"];	
+	}
+}
+class t extends t_base{
+	public $content;
+	public $reply;
+	function bind_reply($retval)
+	{while($row = mysqli_fetch_assoc($retval))
+		{
+		   $reply=new reply;
+		   $reply->bind_reply($row);
+		   $this->reply[]=$reply;
+	
+		}
+		
+		
+	}
+	function bind_info($row)
+	{
+	$this->tid=$row['tid'];
+	$this->type=$row['type'];
+	$this->title=$row['title'];
+	$this->content=$row['content'];
+	if($row['hide_poster']) $this->poster="匿名用户";else $this->poster=$row['poster'];
+	$this->date=$row["date"];	
+	}
+}
+class reply{
+	public $content;
+	public $poster;
+	public $date;
+	function bind_reply($row)
+	{
+		$this->content=$row['content'];
+		$this->date=$row['date'];
+		if($row['hide_poster']) $this->poster="匿名用户";
+		else $this->poster=$row['poster'];
+	
+	}
+}
+?>
