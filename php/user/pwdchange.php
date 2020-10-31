b235df8501e12cf7fe9997e68a91492c28bb2d4c
970005689 <70957110+970005689@users.noreply.github.com>
2020-10-31 22:55:41 +0800

    Update pwdchange.php

--- php/user/pwdchange.php
+++ php/user/pwdchange.php
@@ -3,7 +3,7 @@ include_once('../function.php');
 include_once('../dbconfig.php');
 include_once('../class_response.php');
 $re=new response;
-if(!checklogin()) $re->setf(0,"用户未登录");
+if(!checklogin()) $re->setf(2,"用户未登录");
 if($_GET['password1']=="" or $_GET['password2']=="") $re->setf(0,"原密码或新密码未输入");
 $username=$_SESSION['user'];
 $conn=db_init();
