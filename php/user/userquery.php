d49e766619ef3de477a5e4f76c827b7dacb38b19
970005689 <70957110+970005689@users.noreply.github.com>
2020-11-04 19:15:25 +0800

    Update userquery.php

--- php/user/userquery.php
+++ php/user/userquery.php
@@ -4,6 +4,7 @@ include_once('../function.php');
 include_once('../class_response.php');
 $re=new response;
 if(!checklogin()) $re->setf(2,"用户未登录");
-$re->sets(1,$_SESSION['user']);
+$re->sets(1,"用户已登录");
+$re->binddata_s($_SESSION['user']);
 $re->out();
 ?>
