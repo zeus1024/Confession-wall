e7a1c1cfd8c5f47ec357b1a250dc32fc791832c9
970005689 <70957110+970005689@users.noreply.github.com>
2020-11-11 12:00:54 +0800

    Update pwdchange.php

--- php/user/pwdchange.php
+++ php/user/pwdchange.php
@@ -4,15 +4,15 @@ include_once('../dbconfig.php');
 include_once('../class_response.php');
 $re=new response;
 if(!checklogin()) $re->setf(2,"用户未登录");
-if($_GET['password1']=="" or $_GET['password2']=="") $re->setf(0,"原密码或新密码未输入");
+if($_POST['password1']=="" or $_POST['password2']=="") $re->setf(0,"原密码或新密码未输入");
 $username=$_SESSION['user'];
 $conn=db_init();
 $sql = "SELECT pswhash FROM udb WHERE usn = '$username';";
 $retval=$conn->query($sql);
 $row = mysqli_fetch_assoc($retval);
-if (password_verify($_GET['password1'],$row['pswhash']))
+if (password_verify($_POST['password1'],$row['pswhash']))
 {   
-	$hash=password_hash($_GET['password2'], PASSWORD_DEFAULT);
+	$hash=password_hash($_POST['password2'], PASSWORD_DEFAULT);
 	$sql = "UPDATE udb SET pswhash = '$hash' WHERE usn='$username'; " ;
 	$conn->query($sql);
 	$re->sets(1,"修改成功");
