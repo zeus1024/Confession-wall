40d8c866621c5ed3225f7ccad161715f6ce8216a
yigefanqie-beiyi <70957066+yigefanqie-beiyi@users.noreply.github.com>
2020-12-01 22:04:09 +0800

    Update Changepassword.vue
    修改密码初版

--- src/views/Changepassword.vue
+++ src/views/Changepassword.vue
@@ -1,119 +1,120 @@
+<!-- 修改密码 -->
 <template>
-<div>
-	<div>
-		<Navbar></Navbar>
-
+	<div class="backgroundpicture-changepassword">
+		<div>
+			<!-- 使用navbar组件，上面的导航栏 -->
+			<Navbar></Navbar>
+		</div>
+		<div class="changeinformation">
+			<!-- 输入原密码 -->
+			<van-field  v-model="oldword" label="原密码" />
+			<!-- 输入新密码 -->
+			<van-field v-model="newword" label="新密码" />
+		</div>
+		<div class="changebutton">
+			<!-- 确定修改密码的按钮 -->
+			<van-button type="info" @click="check" color="linear-gradient(to right, #46A3FF, #97cbff)">修改</van-button>
+		</div>
 	</div>
-	<div>
-	<!-- 输入任意文本 -->
-<van-field v-model="oldword" label="原密码" />
-<!-- 输入任意文本 -->
-<van-field v-model="newword" label="新密码" />
-</div>
-<div>
-	<van-button type="info" @click="check">确定修改</van-button>
-</div>
-</div>
 
 </template>
+
 <script >
 	import axios from 'axios';
-	  import { Dialog } from 'vant';
-	  import Navbar from '../components/Navbar.vue';
+	import { Dialog } from 'vant';
+	import Navbar from '../components/Navbar.vue';
 	export default
-{
-  name:'Changepassword',
-  components:{Navbar,},
-  data(){
-    return{
-     oldword:'',
-      newword:'',
-      code:''
-    }
+	{
+		name:'Changepassword',
+		components:{Navbar,},
+		data(){
+			return{
+				oldword:'',
+				newword:'',
+				code:''
+			}
 
-  },
-  methods:{
-  	check(){
-  		if(this.oldword=='' || this.newword=='')
-  		{
-  			Dialog.alert({
-  title: '错误',
-  message: '输入不能为空',
+		},
+		methods:{
+			check(){
+				if(this.oldword=='' || this.newword=='')
+				{
+					Dialog.alert({
+						title: '错误',
+						message: '输入不能为空',
 }).then(() => {  // on close
 });
-  		}
-  		else{this.changepassword()}
-  	},
-  	changepassword(){
-   axios.get('/api/user/pwdchange.php',{       // 还可以直接把参数拼接在url后边
-        withCredentials:true,
-        params:{
-          password1:this.oldword,
-          password2:this.newword,
-        }
-    }).then(res=>{ 
-        this.code = res.data.code;
-        console.log(this.code);
-        if (this.code==1)
+}
+else{this.changepassword()}
+},
+changepassword(){
+	let data = new FormData();
+data.append('password1',this.oldword);
+data.append('password2',this.newword);
+   axios.post('/php/user/pwdchange.php',
+   data,
+   ).then(res=>{ 
+   	this.code = res.data.code;
+ 
+   	if (this.code==1)
 
-      {Dialog.alert({
-  title: '提示',
-  message: '修改密码成功',
-}).then(() => {
-	    	this.$router.push({name:'UserInformation'})
+   		{Dialog.alert({
+   			title: '提示',
+   			message: '修改密码成功',
+   		}).then(() => {
+   			this.$router.push({name:'UserInformation'})
   // on close
 });
-}
-if(this.code==0)
-{Dialog.confirm({
-  title: '提示',
-  message: '修改密码失败，请检查原密码是否正确',
-})
-  .then(() => {
-  		return;
+   	}
+   	if(this.code==0)
+   		{Dialog.confirm({
+   			title: '提示',
+   			message: '修改密码失败，请检查原密码是否正确',
+   		})
+   	.then(() => {
+   		return;
     // on confirm
-  })
- ;}
+})
+   	;}
          // 加上then，res.data就是数据
-    }).catch(error => {
-      console.log(error);
-     
-    });
+     }).catch(error => {
+     Dialog.alert({
+    title: '网路错误',
+    message: error,
+  }).then(() => {
+    return;
+}); 
+
+     });
+ },
 },
-},
-//   	changepassword()
-//     {
-//        axios.post('/api/user/pwdchange.php',{
-//         withCredentials:true,
-//         params:{
-//           Password1:this.oldword,
-//           Password2:this.newword,
-//         }
-//       }).then(res=>{
-//         this.code=res.data.code;
-//         if(this.code==1)
-//         {Dialog.alert({
-//   title: '提示',
-//   message: '修改密码成功',
-// }).then(() => {
-//     this.$router.push({name:'UserInformation'}) ;
-//   // on close
-// })
-// ;           }
-//               }).catch(error => {
-//       console.log(error);
-//       Dialog.alert({
-//   title: '错误',
-//   message: '修改密码错误',
-// }).then(() => {
-//   return;
-//   // on close
-// });
-//     })
-//     },
+
 
 }
 </script>
+
 <style>
-	
+	.backgroundpicture-changepassword{ 
+    height: 100%;
+    width: 100%;
+    background-image: url(https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2193922693,786190290&fm=26&gp=0.jpg) ;
+    position: fixed;
+    background-size: cover;
+  }
+    
+    .changeinformation{
+    	position: fixed;
+    	top: 40%;
+    	width: 80%;
+    	left: 10%;
+    	right: 10%;
+    }
+ 
+    .changebutton{
+    	position: fixed;
+    	top: 60%;
+    	width: 20%;
+    	left: 40%;
+    	right: 40%;
+    }
 </style>
